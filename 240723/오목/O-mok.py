127d829f8de36ea422cda34abcd3c1ed240a7f5c
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-07-23 17:14:09 +0900

    Title: 오목, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240723/오목/O-mok.py
+++ 240723/오목/O-mok.py
@@ -21,7 +21,13 @@ def left(x, y, stand):
     return x, y + 2, True
 
 
-def diagonal(x, y, stand):
+def diagonal_left(x, y, stand):
+    for i in range(5):
+        if stand != arr[x + i][y - i] or in_range(x + i, y - i) == False:
+            return 0,0,False
+    return x + 2, y - 2, True
+
+def diagonal_right(x, y, stand):
     for i in range(5):
         if stand != arr[x + i][y + i] or in_range(x + i, y + i) == False:
             return 0, 0, False
@@ -35,13 +41,19 @@ for i in range(19):
             continue
         stand = arr[i][j]
         x, y, result = down(i, j, stand)
+
         if not result:
             x, y, result = left(i, j, stand)
         if not result:
-            x, y, result = diagonal(i, j, stand)
+            x, y, result = diagonal_left(i, j, stand)
+        if not result:
+            x, y, result = diagonal_right(i, j, stand)
         
         if result:
             break
+        print(i, j)
+        
+
     if result:
         print(stand)
         print(x + 1, y + 1)
