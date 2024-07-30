8675ec12c23c79d32bfd6f520721d18e913e8a4a
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-07-30 19:42:56 +0900

    Title: 특정 구간의 원소 평균값, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
+++ 240730/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -7,12 +7,12 @@ avg_cnt = 0
 for i in range(n):
     for j in range(i, n):
         sum_arr = 0
-        cnt = 0
+        cnt = j + 1 - i
         for k in range(i, j + 1):
             sum_arr += arr[k]
-            cnt += 1
         
-        if sum_arr % cnt == 0 and (sum_arr // cnt) in arr:
+        if sum_arr % cnt == 0 and (sum_arr / cnt) in arr[i: j+1]:
+            #print(i, j, sum_arr / cnt)
             avg_cnt += 1
 
 print(avg_cnt)
