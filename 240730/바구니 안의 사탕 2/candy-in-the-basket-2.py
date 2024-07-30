2042a7e7dd0f6cf28eec321a9e610cb0bedbfc76
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-07-30 22:24:17 +0900

    Title: 바구니 안의 사탕 2, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240730/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -9,13 +9,19 @@ for i in range(n):
 #print(arr[:16])
 sum_max = 0
 
-for i in range(1, MAX_INT - 2 * k + 1):
+
+for i in range(1, MAX_INT + 1):
     sum_candy = 0
-    for j in range(i, i + 2 * k + 1):
+    if i + 2 * k + 1 > MAX_INT:
+        l = MAX_INT
+    else:
+        l = i + 2 * k
+    for j in range(i, l + 1):
         #print(j, end = ' ')
         sum_candy += arr[j]
 
     #print()
     sum_max = max(sum_candy, sum_max)
+
 
 print(sum_max)
