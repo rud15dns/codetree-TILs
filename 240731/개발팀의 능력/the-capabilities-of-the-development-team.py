cfa7ccd4119af2b34a2994a80f2d831661cc7cfb
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-07-31 14:05:04 +0900

    Title: 개발팀의 능력, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,29 @@
+import sys
+sum_diff = sys.maxsize
+
+n = 5
+arr = list(map(int, input().split()))
+
+def sum_op(i, j, k):
+    sum1 = arr[i]
+    sum2 = arr[j] + arr[k]
+    sum3 = sum(arr) - (sum1 + sum2)
+
+    if (sum1 == sum2 or sum2 == sum3 or sum1 == sum3):
+        return -1, -1
+    
+    return max(sum1, sum2, sum3), min(sum1, sum2, sum3)
+
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(n):
+            if k == i or k == j:
+                continue
+            
+            max_sum, min_sum = sum_op(i, j, k)
+            if max_sum < 0 or min_sum < 0:
+                continue
+            sum_diff = min(sum_diff, abs(max_sum - min_sum))
+
+print(sum_diff)
