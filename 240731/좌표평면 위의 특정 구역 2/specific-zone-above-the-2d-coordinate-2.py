b9c92828c862ed130e6e4df9336aefa9afbea2a3
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-07-31 14:45:47 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,31 @@
+max_x = 0
+max_y = 0
+
+min_x = 40000 + 1
+min_y = 40000 + 1
+
+min_sum = min_x * min_y
+
+n = int(input())
+
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        
+        x, y = segments[j]
+
+        max_x = max(max_x, x)
+        max_y = max(max_y, y)
+        min_x = min(min_x, x)
+        min_y = min(min_y, y)
+    
+    print(max_x, max_y, min_x, min_y)
+    min_sum = min(min_sum, (max_x - min_x) * (max_y - min_y))
+
+print(min_sum)
