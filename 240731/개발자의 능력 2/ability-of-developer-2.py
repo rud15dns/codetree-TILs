677e5f516610a8882320085e86365c23621a3cb5
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-07-31 13:45:23 +0900

    Title: 개발자의 능력 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240731/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,25 @@
+import sys
+sum_diff = sys.maxsize
+
+arr = list(map(int, input().split()))
+n = len(arr)
+
+def sum_op(i, j, k, l):
+    sum1 = arr[i] + arr[j]
+    sum2 = arr[k] + arr[l]
+    sum3 = sum(arr) - (sum1 + sum2)
+    
+    return max(sum1, sum2, sum3), min(sum1, sum2, sum3)
+
+
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(n):
+            if k != i:
+                for l in range(k + 1, n):
+                    if l != j:
+                        max_sum, min_sum = sum_op(i, j, k, l)
+                        
+                        sum_diff = min(abs(max_sum - min_sum), sum_diff)
+
+print(sum_diff)
