bc1bf4342aa7b906fcaa71650d5163f930473018
rud15dns <113186906+rud15dns@users.noreply.github.com>
2024-08-07 00:14:56 +0900

    Title: 겹치지 않는 선분 2, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240807/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240807/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -5,17 +5,23 @@ points = [
     for _ in range(n)
 ]
 
+sorted_points = sorted(points)
 cnt = 0
 
 for i in range(n):
-    x1, x2 = points[i]
+    x1, x2 = sorted_points[i]
+    duplicate= False
     for j in range(n):
         if i == j:
             continue
-        xx1, xx2 = points[j]
+        xx1, xx2 = sorted_points[j]
 
         if x1 < xx1 and x2 > xx2:
-            cnt += 2
-    #print(cnt)
+            duplicate = True
+        if xx1 < x1 and xx2 > x1:
+            duplicate = True
+        
+    if duplicate:
+        cnt += 1
 
 print(n - cnt)
